238d94b640e13469a7fa1958add6c9e580846de6
Gonzalez-Mirelis <genoveva@hi.no>
2022-08-29 14:33:34 +0200

    created a new script to implement station/sample filter to the MarVid dataset, as well as to subset the data set to make it more manaegable

---
+++ stationFilters.R
@@ -0,0 +1,8 @@
+---
+title: "species filters for NiN/biotope classification analyses - R Notebook"
+output: html_notebook
+authors: Genoveva Gonzales-Mirelis and Rebecca Ross  
+---
+
+This script reads MarVid data and applies filters on stations/subsamples on the basis of data quality.
+Further, it selects a subsample at random from within all available stations.
