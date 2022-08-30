238bf01aaeb10177905dcbcc5742386b1995ac6b
Gonzalez-Mirelis <genoveva@hi.no>
2022-08-30 10:24:43 +0200

    better annotations, read in all basic input files

--- Config.R
+++ Config.R
@@ -1,19 +1,15 @@
 
-## Station/sample filters
-## By Genoveva Gonzalez Mirelis and Rebecca Ross
-
-# This script reads MarVid data and applies filters on stations/subsamples on the basis of data quality.
-# Further, it selects a subsample at random from within all available stations.
-
 ## Paths
 
 # https://owncloud.imr.no/index.php/s/lCkOERjzJkPItZj
 
 # C:\Users\genoveva\ownCloud\NiN_Mareano\inputs
 
-userName = "C:/Users/genoveva/"
+userName = "C:/Users/genoveva/" # replace with your own name here
 dataPath1 = "ownCloud/NiN_Mareano/inputs"
 
 dataPath <- paste0(userName, dataPath1)
 
 sppdens <- read.csv(file.path(dataPath,"species_densities.csv"))
+refer <- read.table(file.path(dataPath,"reference.csv"), sep ="|", header = TRUE)
+sample_info <- read.csv(file.path(dataPath,"sample_info.csv"))
