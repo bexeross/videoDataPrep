eea47989f97addc9ceb89872fb8ace3f27065438
Gonzalez-Mirelis <genoveva@hi.no>
2022-09-02 13:56:32 +0200

    added path  to taxonary

--- Config.R
+++ Config.R
@@ -6,10 +6,17 @@
 # C:\Users\genoveva\ownCloud\NiN_Mareano\inputs
 
 userName = "C:/Users/genoveva/" # replace with your own name here
+#userName = "C:/Users/genoveva/"
+#userName = "C:/Users/genoveva/"
+  
+  
 dataPath1 = "ownCloud/NiN_Mareano/inputs"
+dataPath2 <- "Havforskningsinstituttet\\Video analysis lab - Reference List\\"
 
 dataPath <- paste0(userName, dataPath1)
 
 sppdens <- read.csv(file.path(dataPath,"species_densities.csv"))
 refer <- read.table(file.path(dataPath,"reference.csv"), sep ="|", header = TRUE)
 sample_info <- read.csv(file.path(dataPath,"sample_info.csv"))
+
+taxonary <- read_xlsx(file.path(dataPath2, "Taxonary.xlsx", sheet = 1))
