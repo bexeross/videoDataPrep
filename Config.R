dc42dad83ca765b90b9b053de5f55646df47828d
bexeross <bexeross@googlemail.com>
2022-09-02 15:35:15 +0200

    make taxonary path work

--- Config.R
+++ Config.R
@@ -1,6 +1,7 @@
 
 ## Paths
 
+library(readxl)
 # https://owncloud.imr.no/index.php/s/lCkOERjzJkPItZj
 
 # C:\Users\genoveva\ownCloud\NiN_Mareano\inputs
@@ -10,13 +11,14 @@ userName = "C:/Users/a21448/" # bex's path
 #userName = "C:/Users/genoveva/"
   
   
-dataPath1 = "ownCloud/NiN_Mareano/inputs"
-dataPath2 <- "Havforskningsinstituttet\\Video analysis lab - Reference List\\"
+dataPath1 <- "ownCloud/NiN_Mareano/inputs"
+dataPath2 <- "Havforskningsinstituttet/Video analysis lab - Reference List"
 
 dataPath <- paste0(userName, dataPath1)
+dataPath_2 <- paste0(userName, dataPath2)
 
 sppdens <- read.csv(file.path(dataPath,"species_densities.csv"))
 refer <- read.table(file.path(dataPath,"reference.csv"), sep ="|", header = TRUE)
 sample_info <- read.csv(file.path(dataPath,"sample_info.csv"))
 
-taxonary <- read_xlsx(file.path(dataPath2, "Taxonary.xlsx", sheet = 1))
+taxonary <- read_xlsx(file.path(dataPath_2, "Taxonary.xlsx"), sheet=1)
