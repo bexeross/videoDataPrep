a915369751f7e3c41e1922205f3d12953ec21fc3
bexeross <bexeross@googlemail.com>
2022-09-16 15:00:26 +0200

    amend access to taxonary impacted by change in userName length

--- Config.R
+++ Config.R
@@ -4,15 +4,16 @@
 library(readxl)
 
 #userName <- "C:/Users/genoveva/OneDrive - Havforskningsinstituttet/General/" # geno's path
-userName <- "C:/Users/a21448/Havforskningsinstituttet/NiN + MAREANO - General" # bex's path
+userName <- "C:/Users/a21448/Havforskningsinstituttet/NiN + MAREANO - General/" # bex's path
 #userName <- "C:/Users/Wilson_Margaret/Havforskningsinstituttet/NiN + MAREANO - General" # Margaret's path
   
   
 dataPath1 <- "inputs"
-dataPath2 <- "Havforskningsinstituttet/Video analysis lab - Reference List"
+dataPath_2 <- "C:/Users/a21448/Havforskningsinstituttet/Video analysis lab - Reference List"
+#dataPath_2 <- "C:/Users/genoveva/Havforskningsinstituttet/Video analysis lab - Reference List"
 
 dataPath <- paste0(userName, dataPath1)
-dataPath_2 <- paste0(userName, dataPath2)
+#dataPath_2 <- paste0(userName, dataPath2)
 
 sppdens <- read.csv(file.path(dataPath,"species_densities.csv"))
 refer <- read.table(file.path(dataPath,"reference.csv"), sep ="|", header = TRUE)
